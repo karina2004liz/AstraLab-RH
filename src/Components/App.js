ff5228c36d5124e3a941d7b02264e3652c77e6f0
BrendaVielmas <breen.vielmas@gmail.com>
2019-12-05 13:54:41 -0600

    primeros componentes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import Home from './Home';
 import Vacancy from './Vacancy';
+import Candidates from './Candidates';
+import Junior from './Junior';
+import Middle from './Middle';
+import Senior from './Senior';
+import NoLevel from './NoLevel';
 import { BrowserRouter as Router, Route, HashRouter, Switch } from 'react-router-dom';
 class App extends Component {
 	render () {
@@ -10,7 +15,12 @@ class App extends Component {
 					<Router>
 						<div className="App">
 							<Route exact path="/" component={Home} />
-							<Route path="/Vacancy" component={Vacancy} />
+							<Route path="/Vacancy" component={Vacancy}/>
+							<Route path="/Candidates" component={Candidates}/>
+							<Route exact path="/Candidates/Junior" component={Junior}/>
+							<Route exact path="/Candidates/Middle" component={Middle}/>
+							<Route exact path="/Candidates/Senior" component={Senior}/>
+							<Route exact path="/Candidates/NoLevel" component={NoLevel}/>
 						</div>
 					</Router>
 				</Switch>
