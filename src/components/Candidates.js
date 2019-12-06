6a912876a1a2cd8493a7ff4e87c6f46a4ecef0b7
BrendaVielmas <breen.vielmas@gmail.com>
2019-12-06 11:40:46 -0600

    css de componente candidates

--- src/components/Candidates.js
+++ src/components/Candidates.js
@@ -7,19 +7,23 @@ import ConsultorIcon from '../AstraLab/consultorIcon.png';
 import SeniorIcon from '../AstraLab/seniorIcon.png';
 import NoLevelIcon from '../AstraLab/noLevelIcon.png';
 class Candidates extends Component {
-   
     render() {
-       
             return (
                 <section className="Candidates">
-                    <div><Link to="/Junior"><img className="imgFilter" src={JuniorIcon}></img></Link></div>
-                    <div><Link to="/Middle"><img className="imgFilter" src={ConsultorIcon}></img></Link></div>
-                    <div><Link to="/Senior"><img className="imgFilter" src={SeniorIcon}></img></Link></div>
-                    <div><Link to="/NoLevel"><img className="imgFilter" src={NoLevelIcon}></img></Link></div>
-                    <div className="returnBtn"><Link to="./">Return</Link></div>
+                    <img className="companyLogo" src={require('../AstraLab/astraLogo.png')} alt="Company logo"></img>
+                    <div className="filterContainer">
+                        <div><Link to="/Junior"><img className="imgFilter" src={JuniorIcon}></img></Link></div>
+                        <div><Link to="/Middle"><img className="imgFilter" src={ConsultorIcon}></img></Link></div>
+                        <div><Link to="/Senior"><img className="imgFilter" src={SeniorIcon}></img></Link></div>
+                        <div><Link to="/NoLevel"><img className="imgFilter" src={NoLevelIcon}></img></Link></div>
+                    </div>
+                    <div className="returnBtnContainer">
+                    <Link to="./">
+                        <button className="returnBtn">RETURN</button>
+                    </Link>
+                    </div>
                 </section>
             )
-        }       
-    
+        }
 }
 export default Candidates;
