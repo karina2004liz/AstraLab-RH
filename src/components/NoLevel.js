3637258e1afdd65f65dac6d40d063fa5ae78ad3e
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-12-06 07:27:57 -0600

    Se agrega componente no level y tabla

---
+++ src/components/NoLevel.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import "firebase/database";
+import fireConfig from './Firebase';
+import { Redirect, Link } from 'react-router-dom';
+
+
+
+class NoLevel extends Component{
+
+
+constructor(){
+    super();
+
+    this.db = fireConfig.database().ref().child("Candidates"); 
+
+    this.state = {
+        candidates:[]
+    };
+
+
+}
+
+componentWillMount(){
+
+    const prevNotes = this.state.candidates;
+
+
+
+    this.db.on("child_added", snap => {
+        prevNotes.push({
+          id: snap.key,
+          email: snap.val().email,
+          levelenglish: snap.val().levelenglish,
+          name: snap.val().name,
+          test: snap.val().testlevel
+        });
+  
+        this.setState({
+          candidates: prevNotes
+        });
+      });
+  
+      this.db.on("child_removed", snap => {
+        for (var i = 0; i < prevNotes.length; i++) {
+          if (prevNotes[i].noteId === snap.key) {
+            prevNotes.splice(i, 1);
+          }
+        }
+  
+        this.setState({
+          candidates: prevNotes
+        });
+      });
+  
+      this.db.on("child_changed", snap => {
+        for (var i = 0; i < prevNotes.length; i++) {
+          if (prevNotes[i].noteId === snap.key) {
+            prevNotes[i] = snap.val();
+          }
+        }
+  
+        this.setState({
+          candidates: prevNotes
+        });
+      });
+}
+
+
+    render(){
+        return(
+
+          <div>
+            
+          <div className="titlejunior" ><h1>No Level</h1></div>
+          
+
+          <div className="juniorDiv" >
+
+            
+          
+            <table class="flat-table">
+              <tbody>
+                <tr>
+                  <th>Name</th>
+                  <th>English Level</th>
+                  <th>Email</th>
+                  <th>Test Level</th>
+                </tr>
+                </tbody>
+            
+          
+
+              {this.state.candidates.map(can =>{
+
+                  if(can.test == "no level")
+
+                  return(
+                      <tr>
+                          <th>{can.name}</th>
+                          <th>{can.levelenglish}</th>
+                          <th>{can.email}</th>
+                          <th>{can.test}</th>
+                      </tr>
+
+
+
+                  )
+              })
+
+              }
+              </table>
+
+      
+          </div>
+
+          <div><Link to="./Candidates">Return</Link></div>
+
+          </div>
+        )
+
+    }
+
+}
+
+export default NoLevel
